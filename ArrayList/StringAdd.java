ceb109162948243cd74def837dee5240628d633d
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-04-22 21:37:03 +0530

    Create StringAdd.java

---
+++ ArrayList/StringAdd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ClassRoom {
+    public static void main(String s[]){
+        ArrayList<String>  list = new ArrayList <String> ();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("WaterMalon");
+        list.add("Lichi");
+        list.add("Mango");
+      System.out.println(list);
+    }
+}
+
+//OutPut
+//[Apple, Banana, WaterMalon, Lichi, Mango]
